70022e14d7678b34192e6d70ee80eff8c9111e61
joosang425 <dkrehd456@naver.com>
2023-12-11 20:54:27 +0900

    #15 - 댓글 관리 서비스의 비즈니스 로직 및 컨트롤러 테스트 작성

---
+++ src/main/java/com/example/boardprojectadmin/dto/response/ArticleCommentClientResponse.java
@@ -0,0 +1,35 @@
+package com.example.boardprojectadmin.dto.response;
+
+import com.example.boardprojectadmin.dto.ArticleCommentDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public record ArticleCommentClientResponse(
+        @JsonProperty("_embedded") Embedded embedded,
+        @JsonProperty("page") Page page
+) {
+
+    public static ArticleCommentClientResponse empty() {
+        return new ArticleCommentClientResponse(
+                new Embedded(List.of()),
+                new Page(1, 0, 1, 0)
+        );
+    }
+
+    public static ArticleCommentClientResponse of(List<ArticleCommentDto> articleComments) {
+        return new ArticleCommentClientResponse(
+                new Embedded(articleComments),
+                new Page(articleComments.size(), articleComments.size(), 1, 0)
+        );
+    }
+
+    public record Embedded(List<ArticleCommentDto> articleComments) {}
+
+    public record Page(
+            int size,
+            long totalElements,
+            int totalPages,
+            int number
+    ) {}
+}
